85fa449c15c4d8043ed25f2af4ef4bf2bdb847a5
Riley <rsbeach97@gmail.com>
2020-11-01 15:24:33 -0700

    shows uv index, partial 5 day forecast, and icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var cityInputEl = document.querySelector("#city-name");
 var userInputEl = document.querySelector("#button-addon2");
 var weatherEl = document.querySelector("#weather-card");
@@ -8,12 +10,19 @@ var windEl = document.getElementById("wind");
 var uvEl = document.getElementById("uv-index");
 var forecast = document.getElementById("future-forecast");
 var date = moment().format('MMMM Do YYYY');
+var weatherIcon = document.createElement("img");
 
-var dayOne = document.getElementById("dayOne");
-var dayTwo = document.getElementById("dayTwo");
-var dayThree = document.getElementById("dayThree");
-var dayFour = document.getElementById("dayFour");
-var dayFive = document.getElementById("dayFive");
+var dayOneTemp = document.getElementById("dayOneTemp");
+var dayTwo = document.getElementById("dayTwoTemp");
+var dayThree = document.getElementById("dayThreeTemp");
+var dayFour = document.getElementById("dayFourTemp");
+var dayFive = document.getElementById("dayFiveTemp");
+
+var dayOneHumidity = document.getElementById("dayOneHumidity");
+var dayTwoHumidity = document.getElementById("dayTwoHumidity");
+var dayThreeHumidity = document.getElementById("dayThreeHumidity");
+var dayFourHumidity = document.getElementById("dayFourHumidity");
+var dayFiveHumidity = document.getElementById("dayFiveHumidity");
 
 //get city weather data
 var apiKey = "9926c56f6fbd3fdacab8dafcc1b3d945";
@@ -37,8 +46,31 @@ var getCityWeather = function(city) {
          .then(json => {
             return displayForecast(json);
          })
+         .catch(err => alert("error"));
         };
-    
+
+
+var getUVIndex = function(json) {
+    return fetch("https://api.openweathermap.org/data/2.5/uvi?lat=" + json.city.coord.lat + "&lon=" + json.city.coord.lon + "&appid=" + apiKey)
+    .then(function (response) {
+        return response.json();
+    }) 
+    .then(function (json) {
+        var uvIndex = json.value;
+        uvEl.textContent = uvIndex;
+
+        if(uvIndex < 3) {
+            uvEl.classList.add("badge-success");
+        } else if(uvIndex >= 3 || uvIndex <= 5) {
+            uvEl.classList.add("badge-primary");
+        } else if(uvIndex >= 6 || uvIndex <= 7 ) {
+            uvEl.classList.add("badge-warning");
+        } else if(uvIndex >= 8 || uvIndex <= 10 ) {
+            uvEl.classList.add("badge-danger");
+        } else
+        uvEl.classList.add("badge-dark");
+    });
+}
 
 //takes user input of city name and inputs it as a parameter for getCityWeather()
 var citySubmit = function(event) {
@@ -63,24 +95,32 @@ var displayWeather = function(json) {
     weatherEl.removeAttribute('hidden');
     //creates h1 of the city's name and current date
     cityHeader.textContent = json.name + " " + date;
+    weatherIcon.src = "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png";
+    cityHeader.appendChild(weatherIcon);
     //display current temperature
     tempEl.textContent = json.main.temp;
     //display humidity
     humidityEl.textContent = json.main.humidity;
     //display wind speed
     windEl.textContent = json.wind.speed + "MPH";
-    //display UV index
-    
 }
 
 
 var displayForecast = function(json) {
     forecast.removeAttribute('hidden');
-    dayOne.textContent = json.list[2].temp;
-    dayTwo.textContent = json.list[10].temp;
-    dayThree.textContent = json.list[18].temp;
-    dayFour.textContent = json.list[26].temp;
-    dayFive.textContent = json.list[34].temp;
+
+    dayOneTemp.textContent = json.list[2].main.temp;
+    dayTwoTemp.textContent = json.list[10].main.temp;
+    dayThreeTemp.textContent = json.list[18].main.temp;
+    dayFourTemp.textContent = json.list[26].main.temp;
+    dayFiveTemp.textContent = json.list[34].main.temp;
+
+    dayOneHumidity.textContent = json.list[2].main.humidity;
+    dayTwoHumidity.textContent = json.list[10].main.humidity;
+    dayThreeHumidity.textContent = json.list[18].main.humidity;
+    dayFourHumidity.textContent = json.list[26].main.humidity;
+    dayFiveHumidity.textContent = json.list[34].main.humidity;
+    getUVIndex(json);
 }
 
 
