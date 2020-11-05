f675c0a2904f8839a99796cbc5283079576d0b9e
Riley <rsbeach97@gmail.com>
2020-11-04 17:57:53 -0700

    finished the js

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,18 @@ var forecast = document.getElementById("future-forecast");
 var date = moment().format('MMMM Do YYYY');
 var weatherIcon = document.createElement("img");
 
+var dateOne =  document.getElementById("dateOne");
+var dateTwo =  document.getElementById("dateTwo");
+var dateThree =  document.getElementById("dateThree");
+var dateFour =  document.getElementById("dateFour");
+var dateFive =  document.getElementById("dateFive");
+
+var iconOne = document.getElementById("iconOne");
+var iconTwo = document.getElementById("iconTwo");
+var iconThree = document.getElementById("iconThree");
+var iconFour = document.getElementById("iconFour");
+var iconFive = document.getElementById("iconFive");
+
 var dayOneTemp = document.getElementById("dayOneTemp");
 var dayTwo = document.getElementById("dayTwoTemp");
 var dayThree = document.getElementById("dayThreeTemp");
@@ -28,7 +40,7 @@ var dayFiveHumidity = document.getElementById("dayFiveHumidity");
 var apiKey = "9926c56f6fbd3fdacab8dafcc1b3d945";
 
 //assembles the api url with the city the user entered and fetches that data
-var getCityWeather = function(city) {
+function getCityWeather(city) {
     var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
     var apiURLForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + apiKey;
     //make a request to the url
@@ -50,7 +62,7 @@ var getCityWeather = function(city) {
         };
 
 
-var getUVIndex = function(json) {
+function getUVIndex(json) {
     return fetch("https://api.openweathermap.org/data/2.5/uvi?lat=" + json.city.coord.lat + "&lon=" + json.city.coord.lon + "&appid=" + apiKey)
     .then(function (response) {
         return response.json();
@@ -73,7 +85,7 @@ var getUVIndex = function(json) {
 }
 
 //takes user input of city name and inputs it as a parameter for getCityWeather()
-var citySubmit = function(event) {
+function citySubmit(event) {
     event.preventDefault();
 
     var cityName = cityInputEl.value.trim();
@@ -98,7 +110,7 @@ var citySubmit = function(event) {
 
     
 
-var displayWeather = function(json) {
+ function displayWeather(json) {
     //toggles weatherEl from hidden to visible to show weather data
     weatherEl.removeAttribute('hidden');
     //creates h1 of the city's name and current date
@@ -114,20 +126,33 @@ var displayWeather = function(json) {
 }
 
 
-var displayForecast = function(json) {
+ function displayForecast(json) {
     forecast.removeAttribute('hidden');
-
+    //TODO: create for loop so this is so much easier lmao 
     dayOneTemp.textContent = json.list[2].main.temp;
     dayTwoTemp.textContent = json.list[10].main.temp;
     dayThreeTemp.textContent = json.list[18].main.temp;
     dayFourTemp.textContent = json.list[26].main.temp;
     dayFiveTemp.textContent = json.list[34].main.temp;
-
+ 
     dayOneHumidity.textContent = json.list[2].main.humidity;
     dayTwoHumidity.textContent = json.list[10].main.humidity;
     dayThreeHumidity.textContent = json.list[18].main.humidity;
     dayFourHumidity.textContent = json.list[26].main.humidity;
     dayFiveHumidity.textContent = json.list[34].main.humidity;
+
+    iconOne.src = "http://openweathermap.org/img/w/" + json.list[2].weather[0].icon + ".png";
+    iconTwo.src = "http://openweathermap.org/img/w/" + json.list[10].weather[0].icon + ".png";
+    iconThree.src = "http://openweathermap.org/img/w/" + json.list[18].weather[0].icon + ".png";
+    iconFour.src = "http://openweathermap.org/img/w/" + json.list[26].weather[0].icon + ".png";
+    iconFive.src = "http://openweathermap.org/img/w/" + json.list[34].weather[0].icon + ".png";
+
+    dateOne.textContent = moment().add(1, 'days');
+    dateTwo.textContent = moment().add(2, 'days');
+    dateThree.textContent = moment().add(3, 'days');
+    dateFour.textContent = moment().add(4, 'days');
+    dateFive.textContent = moment().add(5, 'days');
+
     getUVIndex(json);
 }
 
